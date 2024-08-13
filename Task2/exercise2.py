3031d8fe3a5a1cdd868b12e3bdd057aa1b95475a
zakaryan2107 <zakaryan2107@...>
2024-08-13 15:57:11 +0400

    Task2 Ex2 Finalized

--- Task2/exercise2.py
+++ Task2/exercise2.py
@@ -1,10 +1,19 @@
+import requests
 import feedparser
+import urllib3
+
+# Suppress the InsecureRequestWarning
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def get_headlines(rss_url):
-    feed = feedparser.parse(rss_url)
-    titles = [entry.title for entry in feed.entries]
-    return titles
+    response = requests.get(rss_url, verify=False)  # Disable SSL verification
+    feed = feedparser.parse(response.content)
+    
+    headlines = []
+    for entry in feed.entries:
+        headlines.append(entry.title)
+    
+    return headlines
 
-google_news_url="https://news.google.com/news/rss"
-titles = get_headlines(google_news_url)
-print(titles)
+google_news_url = "https://news.google.com/news/rss"
+print(get_headlines(google_news_url))
