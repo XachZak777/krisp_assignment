3724e7d1a3add37bb67479bd783816573d88c46b
zakaryan2107 <zakaryan2107@...>
2024-08-13 16:12:14 +0400

    Task2 Ex3 Finalized

--- Task2/exercise3.py
+++ Task2/exercise3.py
@@ -1,27 +1,24 @@
-import io
-
 def get_payments_storage():
     return open('/dev/null', 'wb')
 
 def stream_payments_to_storage(storage):
     storage.write(bytes([1, 2, 3]))
 
-class ChecksumStorage(io.BufferedWriter):
-    def __init__(self, storage):
-        self._storage = storage
-        self._checksum = 0
-
-    def write(self, buffer):
-        self._checksum += sum(buffer)
-        return self._storage.write(buffer)
-
-    def get_checksum(self):
-        return self._checksum
-
 def process_payments():
-    storage = get_payments_storage()
-    checksum_storage = ChecksumStorage(storage)
-    stream_payments_to_storage(checksum_storage)
-    print(checksum_storage.get_checksum())
+    checksum = 0
+
+    class ChecksumWrapper:
+        def __init__(self, wrapped):
+            self.wrapped = wrapped
+
+        def write(self, data):
+            nonlocal checksum
+            for byte in data:
+                checksum += byte
+            self.wrapped.write(data)
+
+    storage = ChecksumWrapper(get_payments_storage())
+    stream_payments_to_storage(storage)
+    print(checksum)
 
 process_payments()
